c6e43d2dafe1f5113f4f3cd1538877bb46c6ddcc
EDUARDO ANDREETTA FONTANA <ALU201530349@uniritter.local>
2016-06-10 22:17:15 -0300

    Exercicio

---
+++ Orders.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ExzPedidos
+{
+    public enum OrderStatus
+    {
+        New,
+        Processed,
+        InX,
+        Delivered,
+        Canceled
+    }
+
+    public class Orders
+    {
+        public Guid Id { get; set; }
+        public decimal Total { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime UpdatedOn { get; set; }
+
+
+        private List<Item> items;
+        public List<Item> Items
+        {
+            get
+            {
+                return items ?? new List<Item>();
+            }
+
+            set
+            {
+                items = value;
+            }
+        }
+
+    }
+}
